0048689d10539826a3cd2cbfe3540ae63f043c3f
Joan Jimenez <joanjimenezcamps@gmail.com>
2025-11-26 19:07:40 +0100

    Milestone 3: meteor division,Gameover page layout,hitboxes

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-//selecion de nave
+//selecion de INTRO nave
 if (document.body.id === "page-intro") {
 
   const ships = document.querySelectorAll('.ship');
 
-  ships.forEach(function(ship) {
+  ships.forEach(function (ship) {
 
-    ship.addEventListener('click', function() {
+    ship.addEventListener('click', function () {
       const shipId = ship.getAttribute('data-ship');
       localStorage.setItem('selectedShip', shipId);
       window.location.href = 'game.html';
@@ -19,14 +19,18 @@ if (document.body.id === "page-intro") {
 const game = document.getElementById("game");
 const gameWidth = game.clientWidth;
 const gameHeight = game.clientHeight;
-
+ // JUGADOR
 class Player {
   constructor() {
-    this.width = 60;   
-    this.height = 80;  
+    this.width = 60;
+    this.height = 80;
     this.positionX = gameWidth / 2 - this.width / 2;
     this.positionY = 20;
     this.selectedShip = localStorage.getItem('selectedShip') || 1;
+    this.hitboxOffsetX = 12;
+    this.hitboxOffsetY = 10;
+    this.hitboxWidth = this.width - 24; 
+    this.hitboxHeight = this.height - 20; 
 
     this.updateUI();
   }
@@ -74,14 +78,43 @@ class Player {
 
 
 
-  
 
+//METEORITOS
 class Meteor {
-  constructor() {
-    this.width = 150;   
-    this.height = 150;  
-    this.positionX = Math.random() * (gameWidth - this.width);
-    this.positionY = gameHeight;
+  constructor(size = "big", positionX = null, positionY = null) {
+    this.size = size;
+    if (size === "big") {
+      this.width = 150;
+      this.height = 150;
+      this.health = 3;
+      //hitbox
+      this.hitboxOffsetX = 18;
+      this.hitboxOffsetY = 18;
+      this.hitboxWidth = this.width - 36;
+      this.hitboxHeight = this.height - 36;
+    } else if (size === "medium") {
+      this.width = 60;
+      this.height = 60;
+      this.health = 2;
+      //hitbox
+      this.hitboxOffsetX = 10;
+      this.hitboxOffsetY = 10;
+      this.hitboxWidth = this.width - 20;
+      this.hitboxHeight = this.height - 20;
+    } else {
+      this.width = 40;
+      this.height = 40;
+      this.health = 1;
+      this.directionX = Math.random() < 0.5 ? -1 : 1;
+      this.speedX = 2 + Math.random() * 2;
+      //hitbox
+      this.hitboxOffsetX = 6;
+      this.hitboxOffsetY = 6;
+      this.hitboxWidth = this.width - 12;
+      this.hitboxHeight = this.height - 12;
+    }
+    this.positionX = positionX !== null ? positionX : Math.random() * (gameWidth - this.width);
+    this.positionY = positionY !== null ? positionY : gameHeight;
     this.MeteorELM = null;
 
     this.createMeteor();
@@ -90,7 +123,15 @@ class Meteor {
 
   createMeteor() {
     this.MeteorELM = document.createElement("div");
-    this.MeteorELM.className = "Meteor";
+    this.MeteorELM.className = `Meteor ${this.size}`;
+    // asignacion imagenes meteoritos
+    if (this.size === "big") {
+      this.MeteorELM.style.backgroundImage = `url(../Assets/SetAssets2/meteor.png)`;
+    } else if (this.size === "medium") {
+      this.MeteorELM.style.backgroundImage = `url(../Assets/space_background_pack/asteroid-1.png)`;
+    } else {
+      this.MeteorELM.style.backgroundImage = `url(../Assets/SetAssets2/flaming_meteor.png)`;
+    }
     game.appendChild(this.MeteorELM);
   }
 
@@ -102,37 +143,49 @@ class Meteor {
   }
 
   moveDown() {
-    this.positionY -= 4;
+    let speed = 2;
+    if (this.size === "medium") speed = 4;
+    if (this.size === "small") {
+      speed = 6;
+      this.positionX += this.directionX * this.speedX;
+      if (this.positionX <= 0 || this.positionX >= gameWidth - this.width) {
+      this.directionX *= -1;
+    }
+  }
+    this.positionY -= speed;
     this.updateUI();
-    
   }
 }
-
-class Shooting{
-  constructor(){
+// SHOOTING
+class Shooting {
+  constructor() {
     this.width = 10;
     this.height = 15;
     this.positionX = player.positionX + player.width / 2 - this.width / 2;
     this.positionY = player.positionY + player.height;
+    this.hitboxOffsetX = 0
+    this.hitboxOffsetY = 0
+    this.hitboxWidth = this.width
+    this.hitboxHeight = this.height
     this.ShootingElm = null;
   }
 
-  createShoot(){
-      this.ShootingElm = document.createElement("div");
-      this.ShootingElm.className = "bullet";
-      game.appendChild(this.ShootingElm);
+  createShoot() {
+    this.ShootingElm = document.createElement("div");
+    this.ShootingElm.className = "bullet";
+    game.appendChild(this.ShootingElm);
   }
-    updateUI() {
+  updateUI() {
     this.ShootingElm.style.width = this.width + "px";
     this.ShootingElm.style.height = this.height + "px";
     this.ShootingElm.style.left = this.positionX + "px";
     this.ShootingElm.style.bottom = this.positionY + "px";
   }
-  moveUp(){
-    this.positionY += 6
+  moveUp() {
+    this.positionY += 10
     this.updateUI()
   }
-  
+
 }
 
 
@@ -149,61 +202,78 @@ let keysPressed = {
 };
 
 setInterval(() => {
-  const newMeteor = new Meteor();
+  const newMeteor = new Meteor("big");
   MeteorsArr.push(newMeteor);
-}, 2000);
+}, 5000);
 
+//METEOR COLLISION
 setInterval(() => {
   MeteorsArr.forEach((MeteorInstance, index) => {
     MeteorInstance.moveDown();
 
-    
+
     if (MeteorInstance.positionY + MeteorInstance.height < 0) {
       MeteorInstance.MeteorELM.remove();
       MeteorsArr.splice(index, 1);
+      updatePlanetHealth(10);
     }
-    if (
-      player.positionX < MeteorInstance.positionX + MeteorInstance.width &&
-      player.positionX + player.width > MeteorInstance.positionX &&
-      player.positionY < MeteorInstance.positionY + MeteorInstance.height &&
-      player.positionY + player.height > MeteorInstance.positionY
-    ) {
-      location.href = "gameover.html";
+    if (isCollision(player, MeteorInstance)) {
+       playerHealth -= 1;
+       updatePlayerHealth();
+      MeteorInstance.MeteorELM.remove();
+      MeteorsArr.splice(index, 1);
     }
+    
   });
 }, 50);
 
+//SHOOTING COLLISION AND METEORITES SIZE CREATIONS(BUG DISPAROS FROZEN: FIXED)
 setInterval(() => {
- shootArr.forEach((bullet, bIndex) => {
+  for (let bIndex = shootArr.length - 1; bIndex >= 0; bIndex--) {
+    const bullet = shootArr[bIndex];
     bullet.moveUp();
-   
+
     if (bullet.positionY > gameHeight) {
       bullet.ShootingElm.remove();
       shootArr.splice(bIndex, 1);
-      return;
+      continue;
     }
-    MeteorsArr.forEach((meteor, mIndex) => {
-      if (
-        bullet.positionX < meteor.positionX + meteor.width &&
-        bullet.positionX + bullet.width > meteor.positionX &&
-        bullet.positionY < meteor.positionY + meteor.height &&
-        bullet.positionY + bullet.height > meteor.positionY
-      ) {
-        
+
+    for (let mIndex = MeteorsArr.length - 1; mIndex >= 0; mIndex--) {
+      const meteor = MeteorsArr[mIndex];
+      if (isCollision(bullet, meteor)) {
         bullet.ShootingElm.remove();
         shootArr.splice(bIndex, 1);
 
-        meteor.MeteorELM.remove();
-        MeteorsArr.splice(mIndex, 1);
-        onMeteorDestroyed()
-      }
+        meteor.health -= 1; // RESTA VIDA
 
-    });
-  });
+        if (meteor.health <= 0) {
+          meteor.MeteorELM.remove();
+          MeteorsArr.splice(mIndex, 1);
+          
+
+          if (meteor.size === "big") {
+            for (let i = 0; i < 2; i++) {
+              const offsetX = meteor.positionX + (i === 0 ? -40 : 40);
+              const mediumMeteor = new Meteor("medium", Math.max(0, Math.min(gameWidth - 80, offsetX)), meteor.positionY);
+              MeteorsArr.push(mediumMeteor);
+            }
+          } else if (meteor.size === "medium") {
+            for (let i = 0; i < 3; i++) {
+              const offsetX = meteor.positionX + (i - 1) * 30;
+              const smallMeteor = new Meteor("small", Math.max(0, Math.min(gameWidth - 40, offsetX)), meteor.positionY);
+              MeteorsArr.push(smallMeteor);
+            }
+          }
+          onMeteorDestroyed();
+        }
+        break;
+      }
+    }
+  }
 }, 20);
 
-
-
+//KEY BINDINGS
 document.addEventListener("keydown", (e) => {
   if (e.code === "ArrowLeft") keysPressed.left = true;
   if (e.code === "ArrowRight") keysPressed.right = true;
@@ -224,14 +294,14 @@ document.addEventListener("keydown", (e) => {
   }
 });
 
-  document.addEventListener("keyup", (e) => {
+document.addEventListener("keyup", (e) => {
   if (e.code === "ArrowLeft") keysPressed.left = false;
   if (e.code === "ArrowRight") keysPressed.right = false;
   if (e.code === "ArrowUp") keysPressed.up = false;
   if (e.code === "ArrowDown") keysPressed.down = false;
 });
 
-//  smooth movement
+//  SMOOTH MOVEMENTS
 function smoothPlayerMove() {
   if (keysPressed.left) player.moveLeft();
   if (keysPressed.right) player.moveRight();
@@ -240,7 +310,8 @@ function smoothPlayerMove() {
   requestAnimationFrame(smoothPlayerMove);
 }
 smoothPlayerMove();
-// score
+
+// SCORE
 let score = 0;
 const scoreDisplay = document.getElementById('score');
 
@@ -254,4 +325,52 @@ setInterval(() => {
 
 function onMeteorDestroyed() {
   updateScore(10);
+}
+
+//PLANET HEALTH
+let planetHealth = 100; 
+const planetHealthBar = document.getElementById('planet-health');
+
+function updatePlanetHealth(damage) {
+  planetHealth -= damage;
+  if (planetHealth < 0) planetHealth = 0;
+  planetHealthBar.style.width = planetHealth + "%";
+
+  
+  if (planetHealth <= 30) {
+    planetHealthBar.style.background = "rgba(255, 94, 0, 0.94)"; // Rojo
+  } else if (planetHealth <= 60) {
+    planetHealthBar.style.background = "#ff0"; // Amarillo
+  } else {
+    planetHealthBar.style.background = "#0f0"; // Verde
+  }
+
+  if (planetHealth === 0) {
+    setTimeout(() => {
+      location.href = "gameover.html";
+    }, 500);
+  }
+}
+// PLAYER HEALTH
+let playerHealth = 3;
+const playerHearts = document.querySelectorAll('.heart');
+
+function updatePlayerHealth() {
+  playerHearts.forEach((heart, idx) => {
+    heart.style.opacity = idx < playerHealth ? "1" : "0.2";
+  });
+  if (playerHealth <= 0) {
+    setTimeout(() => {
+      location.href = "gameover.html";
+    }, 500);
+  }
+}
+//hitbox collision
+function isCollision(objA, objB) {
+  return (
+    objA.positionX + objA.hitboxOffsetX < objB.positionX + objB.hitboxOffsetX + objB.hitboxWidth &&
+    objA.positionX + objA.hitboxOffsetX + objA.hitboxWidth > objB.positionX + objB.hitboxOffsetX &&
+    objA.positionY + objA.hitboxOffsetY < objB.positionY + objB.hitboxOffsetY + objB.hitboxHeight &&
+    objA.positionY + objA.hitboxOffsetY + objA.hitboxHeight > objB.positionY + objB.hitboxOffsetY
+  );
 }
